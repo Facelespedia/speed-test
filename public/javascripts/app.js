8064e7d7f2e94ac991ea934e88374590e8781cd5
Narongsak <narongsak.chobsri@gmail.com>
2017-11-27 02:45:18 +0700

    When throw error will refresh page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,28 +4,48 @@ $('#test').on('click', function() {
 		url: '/api/upload-speed',
 		success: function(data){
 			console.log(data);
-			$('#data').append("<div>"+data+"</div>")		
+			if(data == "error") {
+				$("body").html("<p> Your article was successfully added!</p>");
+				window.setTimeout(function(){location.reload()},3000)
+			}else {
+				$('#data').append("<div>"+data+"</div>")		
+			}
 		}
 	});
 	$.ajax({
 		url: '/api/download-speed',
 		success: function(data){
 			console.log(data);
-			$('#data').append("<div>"+data+"</div>")		
+			if(data == "error") {
+				$("body").html("<p> Your article was successfully added!</p>");
+				window.setTimeout(function(){location.reload()},3000)
+			}else {
+				$('#data').append("<div>"+data+"</div>")		
+			}
 		}
 	});
 	$.ajax({
 		url: '/api/ip',
 		success: function(data){
 			console.log(data);
-			$('#data').append("<div>"+data+"</div>")		
+			if(data == "error") {
+				$("body").html("<p> Your article was successfully added!</p>");
+				window.setTimeout(function(){location.reload()},3000)
+			}else {
+				$('#data').append("<div>"+data+"</div>")		
+			}
 		}
 	});
 	$.ajax({
 		url: '/api/ping',
 		success: function(data){
 			console.log(data);
-			$('#data').append("<div>"+data+"</div>")		
+			if(data == "error") {
+				$("body").html("<p> Your article was successfully added!</p>");
+				window.setTimeout(function(){location.reload()},3000)
+			}else {
+				$('#data').append("<div>"+data+"</div>")		
+			}
 		}
 	});
 });
