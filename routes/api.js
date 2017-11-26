75fa7dc96168df73a3ef02e50cf15cab489576a7
Narongsak <narongsak.chobsri@gmail.com>
2017-11-27 02:23:11 +0700

    Add throw error speed test

--- routes/api.js
+++ routes/api.js
@@ -7,24 +7,36 @@ const test = speedTest({maxTime: 5000});
 router.get('/download-speed/', function(req, res, next) {
   speedTest().on('downloadspeed', speed => {
     res.send('Download speed: ' + (speed * 125).toFixed(2) + ' KB/s');    
+  }).on('error', err => {
+    console.log('Speed test error:');
+    console.error(err);
   });
 });
 
 router.get('/upload-speed/', function(req, res, next) {
   speedTest().on('uploadspeed', speed => {
     res.send('Upload speed: ' + (speed * 125).toFixed(2) + ' KB/s');    
+  }).on('error', err => {
+    console.log('Speed test error:');
+    console.error(err);
   });
 });
 
 router.get('/ip/', function(req, res, next) {
   speedTest().on('data', data => {
     res.send('IP: ' + data.client.ip);    
+  }).on('error', err => {
+    console.log('Speed test error:');
+    console.error(err);
   });
 });
 
 router.get('/ping/', function(req, res, next) {
   speedTest().on('data', data => {
     res.send('Ping: ' + data.server.ping);    
+  }).on('error', err => {
+    console.log('Speed test error:');
+    console.error(err);
   });
 });
 
