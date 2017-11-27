69a3768c4e8573173e5644cebe8b749035e33ea7
Narongsak <narongsak.chobsri@gmail.com>
2017-11-27 20:16:30 +0700

    Connect backend to frontend

--- routes/api.js
+++ routes/api.js
@@ -3,42 +3,20 @@ var router = express.Router();
 const speedTest = require('speedtest-net');
 const test = speedTest({maxTime: 5000});
 
-/* GET users listing. */
-router.get('/download-speed/', function(req, res, next) {
-  speedTest().on('downloadspeed', speed => {
-    res.send('Download speed: ' + (speed * 125).toFixed(2) + ' KB/s');    
-  }).on('error', err => {
-    console.log('Download speed test error:');
-    console.error(err);
-    res.send('error');
-  });
-});
-
-router.get('/upload-speed/', function(req, res, next) {
-  speedTest().on('uploadspeed', speed => {
-    res.send('Upload speed: ' + (speed * 125).toFixed(2) + ' KB/s');    
-  }).on('error', err => {
-    console.log('Upload speed test error:');
-    console.error(err);
-    res.send('error');
-  });
-});
-
-router.get('/ip/', function(req, res, next) {
+router.get('/data/', function(req, res, next) {
   speedTest().on('data', data => {
-    res.send('IP: ' + data.client.ip);    
+    var _data = { downloadspeed: data.speeds.download,
+                  uploadspeed: data.speeds.upload,
+                  ip: data.client.ip,
+                  ping: data.server.ping,
+                  sponsor: data.server.sponsor,
+                  location: data.server.location,
+                  country: data.server.country,
+                  isp: data.client.isp };
+    
+    res.send(JSON.stringify(_data));    
   }).on('error', err => {
-    console.log('Ip test error:');
-    console.error(err);
-    res.send('error');
-  });
-});
-
-router.get('/ping/', function(req, res, next) {
-  speedTest().on('data', data => {
-    res.send('Ping: ' + data.server.ping);    
-  }).on('error', err => {
-    console.log('Ping test error:');
+    console.log('Server test error:');
     console.error(err);
     res.send('error');
   });
