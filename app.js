337b4124853f5fdffcea4a4cf5872e64c1a4a83f
narongsakjob <narongsak.chobsri@gmail.com>
2018-11-27 01:44:25 +0700

    Refactor code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -12,7 +11,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view options', { layout: 'layout' });
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,26 @@
-var express = require('express');
-var router = express.Router();
-const speedTest = require('speedtest-net');
-const test = speedTest({maxTime: 5000});
+var express = require('express')
+var router = express.Router()
+const speedTest = require('speedtest-net')
+const test = speedTest({maxTime: 5000})
 
 router.get('/data/', function(req, res, next) {
   speedTest().on('data', data => {
-    var _data = { downloadspeed: data.speeds.download,
-                  uploadspeed: data.speeds.upload,
-                  ip: data.client.ip,
-                  ping: data.server.ping,
-                  sponsor: data.server.sponsor,
-                  location: data.server.location,
-                  country: data.server.country,
-                  isp: data.client.isp };
+    var _data = {
+      downloadspeed: data.speeds.download,
+      uploadspeed: data.speeds.upload,
+      ip: data.client.ip,
+      ping: data.server.ping,
+      sponsor: data.server.sponsor,
+      location: data.server.location,
+      country: data.server.country,
+      isp: data.client.isp
+    }
     
-    res.send(JSON.stringify(_data));    
+    res.send(JSON.stringify(_data)) 
   }).on('error', err => {
-    console.log('Server test error:');
-    console.error(err);
-    res.send('error');
+    console.log('Server test error:')
+    console.error(err)
+    res.send('error')
   });
 });
 
